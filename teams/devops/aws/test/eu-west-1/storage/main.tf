367ee73c5e2da7b0c00f79e2e0e0c3ef7d2dbb7e
Yevhen Lebid <ealebed@gmail.com>
2025-12-16 11:30:07 +0200

    disable Checkov check for testing

--- teams/devops/aws/test/eu-west-1/storage/main.tf
+++ teams/devops/aws/test/eu-west-1/storage/main.tf
@@ -1,5 +1,5 @@
-# checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
 module "s3_bucket" {
+  # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
   source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
 
   environment  = var.environment
