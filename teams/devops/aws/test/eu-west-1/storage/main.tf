6a1aefb6332db6ecbcc63d9af017f9e347530920
Yevhen Lebid <ealebed@gmail.com>
2025-12-16 10:51:35 +0200

    test terraform

--- teams/devops/aws/test/eu-west-1/storage/main.tf
+++ teams/devops/aws/test/eu-west-1/storage/main.tf
@@ -1,14 +1,10 @@
-provider "aws" {
-  region = var.aws_region
+module "s3_bucket" {
+  source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.0"
+
+  environment  = var.environment
+  project_name = var.project_name
 }
 
-# module "core" {
-#   source = "../../modules/core"
-
-#   environment  = var.environment
-#   project_name = var.project_name
+# resource "aws_s3_bucket" "demo" {
+#   bucket = "storage-${var.environment}-531438381462-digger"
 # }
-
-resource "aws_s3_bucket" "demo" {
-  bucket = "storage-${var.environment}-531438381462-digger"
-}
