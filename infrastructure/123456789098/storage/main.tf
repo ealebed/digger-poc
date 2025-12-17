4fba1b52ec0b5ad1f1074b013632abe7c6488f24
Yevhen Lebid <ealebed@gmail.com>
2025-12-17 16:44:28 +0200

    cleanup tested infra

--- infrastructure/123456789098/storage/main.tf
+++ infrastructure/123456789098/storage/main.tf
@@ -1,19 +1,19 @@
-module "s3_bucket_eu" {
-  # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
-  source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
+# module "s3_bucket_eu" {
+#   # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
+#   source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
 
-  providers = { aws = aws.eu }
+#   providers = { aws = aws.eu }
 
-  environment  = "${var.environment}-${var.aws_account_id}-eu"
-  project_name = var.project_name
-}
+#   environment  = "${var.environment}-${var.aws_account_id}-eu"
+#   project_name = var.project_name
+# }
 
-module "s3_bucket_us" {
-  # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
-  source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
+# module "s3_bucket_us" {
+#   # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
+#   source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
 
-  providers = { aws = aws.us }
+#   providers = { aws = aws.us }
 
-  environment  = "${var.environment}-${var.aws_account_id}-us"
-  project_name = var.project_name
-}
+#   environment  = "${var.environment}-${var.aws_account_id}-us"
+#   project_name = var.project_name
+# }
