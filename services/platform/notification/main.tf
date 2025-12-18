2860ae26df8b90706943a602816b4a53ed6efdd5
Yevhen Lebid <ealebed@gmail.com>
2025-12-18 09:31:31 +0200

    update workflows

--- services/platform/notification/main.tf
+++ services/platform/notification/main.tf
@@ -2,6 +2,6 @@ module "s3_bucket" {
   # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
   source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
 
-  environment  = "${var.aws_account_id}-${var.environment}-${var.region}"
+  environment  = "${var.aws_account_id}-${var.service_name}-${var.environment}-${var.region}"
   project_name = var.service_name
 }
