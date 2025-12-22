a853663da04a106036c02a438e7b35db45c3da9a
Yevhen Lebid <ealebed@gmail.com>
2025-12-22 15:14:41 +0200

    cleanup resources; one more test (#26)
    auto-merge

--- teams/devops/aws/test/eu-west-1/storage/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "tf-state-531438381462-digger"
-    key          = "devops/aws/test/eu-west-1/storage/terraform.tfstate"
-    region       = "eu-west-1"
-    use_lockfile = true
-  }
-}
