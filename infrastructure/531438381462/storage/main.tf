39f73f5472b6b807d463c2e0e69b13aea2ba2a20
Yevhen Lebid <ealebed@gmail.com>
2025-12-23 10:13:54 +0200

    test auth

--- infrastructure/531438381462/storage/main.tf
+++ infrastructure/531438381462/storage/main.tf
@@ -1,9 +1,9 @@
-module "s3_bucket" {
-  # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
-  source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
+# module "s3_bucket" {
+#   # checkov:skip=CKV_TF_1: module source is pinned to a trusted tag; commit SHA pinning is handled by release process
+#   source = "git::https://github.com/ealebed/terraform-modules.git?ref=s3-bucket/v1.0.1"
 
-  providers = { aws = aws.eu }
+#   providers = { aws = aws.eu }
 
-  environment  = "${var.environment}-${var.aws_account_id}"
-  project_name = var.project_name
-}
+#   environment  = "${var.environment}-${var.aws_account_id}"
+#   project_name = var.project_name
+# }
